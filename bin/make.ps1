30df76c999cec7453c2d5fcc7b18479193ff1255
Andrey Helldar <helldar@dragon-code.pro>
2026-05-28 17:11:55 +0300

    Remove HTML comments in make.ps1 and make.sh

--- bin/make.ps1
+++ bin/make.ps1
@@ -16,6 +16,8 @@ function Remove-UnwantedTags {
     $content = [System.Text.RegularExpressions.Regex]::Replace($content, '(?is)<(style|script|form)\b[^>]*>.*?</\1\s*>', '')
     # Remove void/self-closing tags: meta, link
     $content = [System.Text.RegularExpressions.Regex]::Replace($content, '(?is)<(meta|link)\b[^>]*/?>', '')
+    # Remove HTML comments
+    $content = [System.Text.RegularExpressions.Regex]::Replace($content, '(?s)<!--.*?-->', '')
 
     $utf8WithoutBom = New-Object System.Text.UTF8Encoding $false
     [System.IO.File]::WriteAllText($Path, $content, $utf8WithoutBom)
--- bin/make.sh
+++ bin/make.sh
@@ -14,6 +14,7 @@ remove_unwanted_tags() {
     perl -0777 -pe '
         s{<(style|script|form)\b[^>]*>.*?</\1\s*>}{}gis;
         s{<(meta|link)\b[^>]*/?>}{}gis;
+        s{<!--.*?-->}{}gs;
     ' "$file" > "$temp_file"
     mv "$temp_file" "$file"
 }
