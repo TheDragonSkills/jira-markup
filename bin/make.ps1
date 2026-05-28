4ea56bf222888091436c54176baf5a415de0904d
Andrey Helldar <helldar@dragon-code.pro>
2026-05-28 10:47:59 +0300

    Updated make commands

--- bin/make.ps1
+++ bin/make.ps1
@@ -6,7 +6,17 @@ $repositoryRoot = Split-Path -Parent $scriptDirectory
 
 Push-Location -LiteralPath $repositoryRoot
 try {
-    curl -o source/help_page.html https://ouryahoo.atlassian.net/secure/WikiRendererHelpAction.jspa?section=all
+    curl -o source/texteffects.html https://ouryahoo.atlassian.net/secure/WikiRendererHelpAction.jspa?section=texteffects
+    curl -o source/headings.html https://ouryahoo.atlassian.net/secure/WikiRendererHelpAction.jspa?section=headings
+    curl -o source/breaks.html https://ouryahoo.atlassian.net/secure/WikiRendererHelpAction.jspa?section=breaks
+    curl -o source/links.html https://ouryahoo.atlassian.net/secure/WikiRendererHelpAction.jspa?section=links
+    curl -o source/lists.html https://ouryahoo.atlassian.net/secure/WikiRendererHelpAction.jspa?section=lists
+    curl -o source/images.html https://ouryahoo.atlassian.net/secure/WikiRendererHelpAction.jspa?section=images
+    curl -o source/attachments.html https://ouryahoo.atlassian.net/secure/WikiRendererHelpAction.jspa?section=attachments
+    curl -o source/tables.html https://ouryahoo.atlassian.net/secure/WikiRendererHelpAction.jspa?section=tables
+    curl -o source/advanced.html https://ouryahoo.atlassian.net/secure/WikiRendererHelpAction.jspa?section=advanced
+    curl -o source/miscellaneous.html https://ouryahoo.atlassian.net/secure/WikiRendererHelpAction.jspa?section=miscellaneous
+    
     codex '$aif-distillation source --split --path skills --redact-source-map --name jira-markup'
 }
 finally {
--- bin/make.sh
+++ bin/make.sh
@@ -6,5 +6,15 @@ repository_root=$(dirname "$script_dir")
 
 cd "$repository_root"
 
-curl -o source/help_page.html https://ouryahoo.atlassian.net/secure/WikiRendererHelpAction.jspa?section=all
+curl -o source/texteffects.html https://ouryahoo.atlassian.net/secure/WikiRendererHelpAction.jspa?section=texteffects
+curl -o source/headings.html https://ouryahoo.atlassian.net/secure/WikiRendererHelpAction.jspa?section=headings
+curl -o source/breaks.html https://ouryahoo.atlassian.net/secure/WikiRendererHelpAction.jspa?section=breaks
+curl -o source/links.html https://ouryahoo.atlassian.net/secure/WikiRendererHelpAction.jspa?section=links
+curl -o source/lists.html https://ouryahoo.atlassian.net/secure/WikiRendererHelpAction.jspa?section=lists
+curl -o source/images.html https://ouryahoo.atlassian.net/secure/WikiRendererHelpAction.jspa?section=images
+curl -o source/attachments.html https://ouryahoo.atlassian.net/secure/WikiRendererHelpAction.jspa?section=attachments
+curl -o source/tables.html https://ouryahoo.atlassian.net/secure/WikiRendererHelpAction.jspa?section=tables
+curl -o source/advanced.html https://ouryahoo.atlassian.net/secure/WikiRendererHelpAction.jspa?section=advanced
+curl -o source/miscellaneous.html https://ouryahoo.atlassian.net/secure/WikiRendererHelpAction.jspa?section=miscellaneous
+
 codex "\$aif-distillation source --split --path skills --redact-source-map --name jira-markup"
